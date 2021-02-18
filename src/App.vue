54cde6caf1722b2c6e14a761282e18400cdc1be4
Chanda Mulenga <dev@stctheproducer.com>
2021-02-18 12:54:03 +0200

    feat: add fleet.csv

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
+<!-- @format -->
+
 <template>
   <div class="container" id="app">
     <div class="row">
@@ -6,43 +8,42 @@
         <h1>Vue CSV</h1>
 
         <h2>Example Template</h2>
-        <p>You can download <a href="/example.csv">this template</a> for your convenience.</p>
+        <p>
+          You can download <a href="/example.csv">this template</a> for your
+          convenience.
+        </p>
         <form @submit.prevent="onSubmit">
-          <!--          :map-fields="['name', 'age']"-->
+          <!-- :map-fields="{name: 'Name', age: 'Age'}" -->
           <vue-csv-import
-              :headers="['Name', 'Age']"
-              :map-fields="{name: 'Name', age: 'Age'}"
-              :can-ignore="false"
-              :file-mime-types="['text/csv']"
-              auto-match-fields
-              auto-match-ignore-case
-              table-class="u-full-width"
-              v-model="csv"
+            :headers="['Name', 'Age']"
+            :map-fields="['name', 'age']"
+            :can-ignore="false"
+            :file-mime-types="['text/csv']"
+            auto-match-fields
+            auto-match-ignore-case
+            table-class="u-full-width"
+            v-model="csv"
           >
-
             <template slot="next" slot-scope="{ load }">
               <div class="d-flex justify-content-end">
-                <button
-                    @click.prevent="load"
-                    v-if="!csv.length > 0"
-                >Load File
-                </button
-                >
+                <button @click.prevent="load" v-if="!csv.length > 0">
+                  Load File
+                </button>
               </div>
             </template>
           </vue-csv-import>
           <table class="u-full-width">
             <thead>
-            <tr>
-              <th>Name</th>
-              <th>Age</th>
-            </tr>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+              </tr>
             </thead>
             <tbody>
-            <tr :key="index" v-for="(person, index) in csv">
-              <td>{{ person.name }}</td>
-              <td>{{ person.age }}</td>
-            </tr>
+              <tr :key="index" v-for="(person, index) in csv">
+                <td>{{ person.name }}</td>
+                <td>{{ person.age }}</td>
+              </tr>
             </tbody>
           </table>
 
@@ -56,22 +57,22 @@
 </template>
 
 <script>
-    import {VueCsvImport} from 'vue-csv-import'
+  import { VueCsvImport } from 'vue-csv-import'
 
-    export default {
-        name: 'App',
-        components: {
-            VueCsvImport
-        },
+  export default {
+    name: 'App',
+    components: {
+      VueCsvImport,
+    },
 
-        data: () => ({
-            csv: []
-        }),
+    data: () => ({
+      csv: [],
+    }),
 
-        methods: {
-            onSubmit() {
-                alert(`Data entered: ${JSON.stringify(this.csv)}`)
-            }
-        }
-    }
+    methods: {
+      onSubmit() {
+        alert(`Data entered: ${JSON.stringify(this.csv)}`)
+      },
+    },
+  }
 </script>
