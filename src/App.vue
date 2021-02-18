722b1cde4e2dfb26c91e9b7a7bd89d31c98e964e
Chanda Mulenga <dev@stctheproducer.com>
2021-02-18 13:45:03 +0200

    feat: fleet addition update

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,39 @@
 
         <h2>Example Template</h2>
         <p>
-          You can download <a href="/example.csv">this template</a> for your
+          You can download <a href="/fleet.csv">this template</a> for your
           convenience.
         </p>
         <form @submit.prevent="onSubmit">
-          <!-- :map-fields="{name: 'Name', age: 'Age'}" -->
           <vue-csv-import
-            :headers="['Name', 'Age']"
-            :map-fields="['name', 'age']"
+            :headers="[
+              'Vehicle Make',
+              'Vehicle Model',
+              'Year Of Make',
+              'Registration Number',
+              'Engine Number',
+              'Chassis Number',
+              'Color',
+              'Cubic Capacity',
+              'Seating Capacity',
+              'Body Type',
+              'Certificate Number',
+              'Vehicle Class',
+            ]"
+            :map-fields="{
+              vehicleMake: 'Vehicle Make',
+              vehicleModel: 'Vehicle Model',
+              yearOfManufacture: 'Year Of Make',
+              regNumber: 'Registration Number',
+              engineNumber: 'Engine Number',
+              chassisNumber: 'Chassis Number',
+              color: 'Color',
+              cubicCapacity: 'Cubic Capacity',
+              seatingCapacity: 'Seating Capacity',
+              bodyType: 'Body Type',
+              certificateNumber: 'Certificate Number',
+              productType: 'Vehicle Class',
+            }"
             :can-ignore="false"
             :file-mime-types="['text/csv']"
             auto-match-fields
@@ -35,14 +60,34 @@
           <table class="u-full-width">
             <thead>
               <tr>
-                <th>Name</th>
-                <th>Age</th>
+                <th>Vehicle Make</th>
+                <th>Vehicle Model</th>
+                <th>Year Of Make</th>
+                <th>Registration Number</th>
+                <th>Engine Number</th>
+                <th>Chassis Number</th>
+                <th>Color</th>
+                <th>Cubic Capacity</th>
+                <th>Seating Capacity</th>
+                <th>Body Type</th>
+                <th>Certificate Number</th>
+                <th>Vehicle Class</th>
               </tr>
             </thead>
             <tbody>
-              <tr :key="index" v-for="(person, index) in csv">
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
+              <tr :key="index" v-for="(item, index) in csv">
+                <td>{{ item.vehicleMake }}</td>
+                <td>{{ item.vehicleModel }}</td>
+                <td>{{ item.yearOfManufacture }}</td>
+                <td>{{ item.regNumber }}</td>
+                <td>{{ item.engineNumber }}</td>
+                <td>{{ item.chassisNumber }}</td>
+                <td>{{ item.color }}</td>
+                <td>{{ item.cubicCapacity }}</td>
+                <td>{{ item.seatingCapacity }}</td>
+                <td>{{ item.bodyType }}</td>
+                <td>{{ item.certificateNumber }}</td>
+                <td>{{ item.productType }}</td>
               </tr>
             </tbody>
           </table>
@@ -50,7 +95,7 @@
           <button type="submit">Submit CSV</button>
         </form>
 
-        <pre><code>{{ csv }}</code></pre>
+        <!-- <pre><code>CSV: {{ csv }}</code></pre> -->
       </div>
     </div>
   </div>
